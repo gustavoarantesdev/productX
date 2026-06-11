bdcc922c9e544dff19cffb93cc173f075d2fd874
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-06-11 00:01:17 -0300

    feat: adiciona componentes do layout principal

---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,5 @@
+export default function NavBar() {
+  return (
+    <header className="flex h-16 items-center justify-between border-b border-gray-300 bg-white px-8"></header>
+  );
+}
---
+++ src/components/layout/SideBar.tsx
@@ -0,0 +1,26 @@
+import { PackageOpen } from "lucide-react";
+
+export default function SideBar() {
+  return (
+    <aside className="flex h-screen w-64 flex-col overflow-y-auto border-r border-gray-300 bg-white">
+      {/* Logo */}
+      <div className="flex h-16 items-center justify-center">
+        <span className="rounded-lg bg-blue-400 p-1.5">
+          <PackageOpen size={20} color="white" />
+        </span>
+        <span className="ml-2 text-lg font-semibold">ProductX</span>
+      </div>
+
+      <hr className="mx-auto w-55 text-gray-300" />
+
+      {/* Links */}
+      <div className="p-4">
+        <ul className="mx-auto mt-3 space-y-4">
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+}
