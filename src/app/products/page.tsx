8869cf733c8ac4d618e1f9848ee38c5212897a1b
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-06-11 15:22:01 -0300

    feat: adiciona listagem de produtos

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,29 @@
 import PageTitle from "@/components/ui/PageTitle";
+import { prisma } from "@/lib/prisma";
+import { Plus } from "lucide-react";
 
 export default async function ProductsPage() {
+  const products = await prisma.product.findMany({
+    where: { isActive: true },
+    orderBy: { createdAt: "desc" },
+  });
+
+  if (products.length === 0) {
+    return (
+      <div>
+        <PageTitle title="Produtos" />
+        <div className="p-4 pt-0">
+          <p className="text-red-400 pb-4 text-sm">
+            Nenhum produto cadastrado.
+          </p>
+          <button className="flex items-center py-2 px-4 bg-blue-500 rounded-lg text-white cursor-pointer hover:bg-blue-600">
+            <Plus className="mr-1" size={20} />
+            Cadastre
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <PageTitle title="Produtos" />
