2afa3f731cf776cdd7b7cec288baf90c49c74539
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-06-11 10:23:46 -0300

    feat: adiciona página para produtos

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import PageTitle from "@/components/ui/PageTitle";
+
+export default async function ProductsPage() {
+  return (
+    <div>
+      <PageTitle title="Produtos" />
+    </div>
+  );
+}
