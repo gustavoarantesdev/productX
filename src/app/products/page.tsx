c6496d6ffa8daaa7d8a3ad52040d73f2c7cb11f0
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-06-11 23:21:29 -0300

    feat: adiciona tabela de listagem de produtos

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,7 @@
 import PageTitle from "@/components/ui/PageTitle";
 import { prisma } from "@/lib/prisma";
-import { Plus } from "lucide-react";
+import { Ellipsis, Plus } from "lucide-react";
+import Link from "next/link";
 
 export default async function ProductsPage() {
   const products = await prisma.product.findMany({
@@ -13,13 +14,17 @@ export default async function ProductsPage() {
       <div>
         <PageTitle title="Produtos" />
         <div className="p-4 pt-0">
-          <p className="text-red-400 pb-4 text-sm">
+          <p className="pb-4 text-sm text-red-400">
             Nenhum produto cadastrado.
           </p>
-          <button className="flex items-center py-2 px-4 bg-blue-500 rounded-lg text-white cursor-pointer hover:bg-blue-600">
+
+          <Link
+            href={"/products/create"}
+            className="flex w-fit cursor-pointer items-center rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          >
             <Plus className="mr-1" size={20} />
             Cadastre
-          </button>
+          </Link>
         </div>
       </div>
     );
@@ -27,6 +32,54 @@ export default async function ProductsPage() {
   return (
     <div>
       <PageTitle title="Produtos" />
+
+      <div className="mx-4 w-xl overflow-x-auto rounded-lg border border-gray-300 shadow-sm">
+        <table className="w-full text-left text-sm text-gray-600">
+          <thead className="bg-gray-100 text-sm">
+            <tr>
+              <th scope="col" className="px-6 py-3 font-medium">
+                Nome
+              </th>
+              <th scope="col" className="px-6 py-3 font-medium">
+                Preço
+              </th>
+              <th scope="col" className="px-6 py-3 font-medium">
+                Estoque
+              </th>
+              <th scope="col" className="px-6 py-3 font-medium"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr
+                key={product.id}
+                className="border-t border-gray-300 hover:bg-gray-50"
+              >
+                <td className="px-6 py-3 font-medium whitespace-nowrap text-black">
+                  {product.name}
+                </td>
+                <td className="px-6 py-3">R$ {product.price.toString()}</td>
+                <td className="px-6 py-3">{product.stock}</td>
+                <td className="px-6 py-3">
+                  <button className="cursor-pointer rounded-lg bg-gray-200 p-1 hover:bg-gray-300">
+                    <Ellipsis size={20} />{" "}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="p-4">
+        <Link
+          href={"/products/create"}
+          className="my-trasition flex w-fit cursor-pointer items-center rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+        >
+          <Plus className="mr-1" size={20} />
+          Cadastre
+        </Link>
+      </div>
     </div>
   );
 }
