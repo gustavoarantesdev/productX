3265c6d917a43920d84fc722d0cae1905b003db3
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-06-11 23:21:53 -0300

    feat: adiciona tela de formulário para cadastrar produto

--- src/app/products/create/page.tsx
+++ src/app/products/create/page.tsx
@@ -1,24 +1,82 @@
 import PageTitle from "@/components/ui/PageTitle";
-import { createProduct } from "../actions";
+import { createProduct } from "./actions";
+import Link from "next/link";
 
 export default function CreateProductPage() {
   return (
     <div>
       <PageTitle title="Cadastrar Produto" />
 
-      <form action={createProduct} className="gap-y-3 w-full flex flex-col">
-        <div className="">
-          <label htmlFor="name" className="font-medium mb-2">
-            Nome
-            <span className="font-bold text-red-500 ml-1">*</span>
-          </label>
-          <input type="text" id="name" required className="my-input" />
+      <form action={createProduct} className="flex w-full flex-col px-4">
+        <label htmlFor="name" className="mb-2 block font-medium">
+          Nome
+          <span className="ml-1 font-bold text-red-500">*</span>
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          className="my-input mb-2 w-lg"
+          autoFocus
+          autoComplete="off"
+        />
 
-          <label htmlFor="price" className="font-medium mb-2">
-            Preço
-            <span className="font-bold text-red-500 ml-1">*</span>
-          </label>
-          <input type="number" id="price" required className="my-input" />
+        <div className="mb-2 flex w-lg gap-x-4">
+          <div className="w-full">
+            <label htmlFor="price" className="mb-2 block font-medium">
+              Preço
+              <span className="ml-1 font-bold text-red-500">*</span>
+            </label>
+            <input
+              type="string"
+              name="price"
+              id="price"
+              required
+              className="my-input"
+              min={0}
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="">
+            <label htmlFor="stock" className="mb-2 block font-medium">
+              Estoque
+              <span className="ml-1 font-bold text-red-500">*</span>
+            </label>
+            <input
+              type="number"
+              id="stock"
+              name="stock"
+              required
+              className="my-input"
+              min={0}
+            />
+          </div>
+        </div>
+
+        <label htmlFor="description" className="mb-2 block font-medium">
+          Descrição
+        </label>
+        <textarea
+          name="description"
+          id="description"
+          className="my-input mb-4 w-lg"
+        ></textarea>
+
+        <div className="flex w-lg justify-end gap-x-4">
+          <Link
+            href={"/products"}
+            className="my-transition flex w-fit cursor-pointer items-center rounded-lg bg-gray-200 px-4 py-2 font-medium text-gray-600 hover:bg-gray-300"
+          >
+            Retornar
+          </Link>
+          <button
+            type="submit"
+            className="my-transition cursor-pointer items-center rounded-lg bg-green-600 px-4 py-2 text-center text-white hover:bg-green-700"
+          >
+            Cadastrar
+          </button>
         </div>
       </form>
     </div>
